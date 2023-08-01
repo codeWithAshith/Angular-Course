6cc4b42870b019721e61373c88f745dd34342e61
codewithashith <codewithashith@gmail.com>
2023-08-01 12:01:27 +0530

    02. String Interpolation

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
 })
 export class CourseComponent {
-
+  title = 'Courses Component';
 }
