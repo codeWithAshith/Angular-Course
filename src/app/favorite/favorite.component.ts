fe11f879e75265fcd2aa158cc509f4b19b8e4c27
codewithashith <codewithashith@gmail.com>
2023-08-01 16:34:14 +0530

    11. Using Alias

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css'],
 })
 export class FavoriteComponent implements OnInit {
-  @Input() passedValue?: string;
+  @Input("usingAlias") passedValue?: string;
 
   constructor() {}
 
