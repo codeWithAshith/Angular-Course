0d770acbddcc150adbf45adc96e138cbc6f052fd
codewithashith <codewithashith@gmail.com>
2023-08-01 16:31:49 +0530

    10. Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { CourseComponent } from './course/course.component';
 import { CourseService } from './course.service';
 import { FormsModule } from '@angular/forms';
 import { SummaryPipe } from './summary.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseComponent, SummaryPipe],
+  declarations: [AppComponent, CourseComponent, SummaryPipe, FavoriteComponent],
   imports: [BrowserModule, FormsModule],
   providers: [CourseService],
   bootstrap: [AppComponent],
