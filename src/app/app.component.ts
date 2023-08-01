8127fd28862a4e039750786cb23b9a8d75a43b34
codewithashith <codewithashith@gmail.com>
2023-08-01 18:09:48 +0530

    14. Directives-ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  courses = [];
+  viewMode = 'map';
+
+  onClick(mode: string) {
+    this.viewMode = mode;
+  }
 }
