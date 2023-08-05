a540b5aab6da47747f94b8b3b20ad90aba209613
codewithashith <codewithashith@gmail.com>
2023-08-05 17:02:26 +0530

    38. Routing - params

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
 export class UserDetailsComponent {
+  id: any = 0;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      this.id = params.get('userId');
+    });
+  }
 }
